f72cc9b836c6db6e4c5bda2a9663b4143dc57ba4
Yulia <yulia.blyndiuk@gmail.com>
2017-06-19 15:22:36 +0300

    preparation

--- tests.js
+++ tests.js
@@ -9,3 +9,34 @@ const {app, runServer, closeServer}  = require('../server');
 const {TEST_DATABASE_URL} = require('../config');
 
 chai.use(chaiHttp);
+
+function seedBlogPostData() {
+	console.log('seeding restaurant data');
+	const seedData = [];
+
+	for (let i = 1; i <=10; i++) {
+		seedData.push(generateBlogPostData());
+	}
+	return BlogPost.insertMany(seedData);
+}
+
+function generateContent() {
+	const contents = ['this is randon blogpost content', 'this is another random blog post content',
+	'here goes one more random content'];
+	return contents[Math.floor(Math.random() * contents.length)];
+}
+
+function generateTitle() {
+	const titles = ['title1', 'title2', 'title3', 'title4'];
+	return titles[Math.floor(Math.random() * titles.length)];
+}
+
+function generateBlogPostData() {
+	return {
+	author: faker.author.authorName(),
+    content: generateContent(),
+    title: generateTitle(),
+	}
+}
+
+console.log(seedBlogPostData);
