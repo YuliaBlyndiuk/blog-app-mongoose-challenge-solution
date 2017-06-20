5f8ec2b3141b646f729ddda6f8f00bfc909946c0
Yulia <yulia.blyndiuk@gmail.com>
2017-06-20 11:18:48 +0300

    added test for PUT request

--- tests.js
+++ tests.js
@@ -125,33 +125,33 @@ describe('blog API resource', function() {
 			});
 		});
 	});
-	// describe('PUT endpoint', function() {
-	// 	it('should update fields one sends over', function() {
-	// 		const updData = {
-	// 			title: 'testTitle',
-	// 			content: 'testContent'
-	// 		};
+	describe('PUT endpoint', function() {
+		it('should update fields one sends over', function() {
+			const updData = {
+				title: 'testTitle',
+				content: 'testContent'
+			};
 
-	// 		return BlogPost
-	// 		.findOne()
-	// 		.exec()
-	// 		.then(function(post) {
-	// 			updData.id = post.id;
-	// 			return chai.request(app)
-	// 			.put('/posts/${post.id}')
-	// 			.send(updData);
-	// 		})
+			return BlogPost
+			.findOne()
+			.exec()
+			.then(function(post) {
+				updData.id = post.id;
+				return chai.request(app)
+				.put(`/posts/${post.id}`)
+				.send(updData);
+			})
 
-	// 		.then(function(res) {
-	// 			res.should.have.status(204);
-	// 			return BlogPost.findById(updData.id).exec();
-	// 		})
-	// 		.then(function(post) {
-	// 			post.title.should.equal(updData.title);
-	// 			post.content.should.equal(updData.content);
-	// 		});
-	// 	});
-	// });
+			.then(function(res) {
+				res.should.have.status(201);
+				return BlogPost.findById(updData.id).exec();
+			})
+			.then(function(post) {
+				post.title.should.equal(updData.title);
+				post.content.should.equal(updData.content);
+			});
+		});
+	});
 
 	describe('DELETE endpoint', function() {
 		it('Delete a blogpost by id', function() {
